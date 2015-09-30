4c7920dd2a2a0a912512ddd16780f537522b00f8
APikielny <apikielny@gmail.com>
2015-09-30 13:42:27 -0400

    Updated from Brython Server: September 30, 2015 1:42:28 PM EDT

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: Eric
-Credit: Me!
+Author: Adam Pikielny
+Credit: http://stackoverflow.com/questions/493386/how-to-print-in-python-without-newline-or-space
 
 Assignment:
 
@@ -25,3 +25,9 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+import sys
+
+stringinput=list(input("Please enter a string of text (the bigger the better): "))
+stringinput.reverse()
+for x in stringinput:
+    sys.stdout.write(x)
