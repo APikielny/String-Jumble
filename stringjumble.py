6d76f90b79da27911b68d5d2537db344576a0a58
APikielny <apikielny@gmail.com>
2015-09-30 13:57:18 -0400

    Updated from Brython Server: September 30, 2015 1:57:20 PM EDT

--- stringjumble.py
+++ stringjumble.py
@@ -2,6 +2,7 @@
 stringjumble.py
 Author: Adam Pikielny
 Credit: http://stackoverflow.com/questions/493386/how-to-print-in-python-without-newline-or-space
+http://stackoverflow.com/questions/743806/split-string-into-a-list-in-python
 
 Assignment:
 
@@ -27,7 +28,17 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 import sys
 
-stringinput=list(input("Please enter a string of text (the bigger the better): "))
-stringinput.reverse()
-for x in stringinput:
+stringinput=(input("Please enter a string of text (the bigger the better): "))
+liststringinput=list(stringinput)
+liststringinput.reverse()
+for x in liststringinput:
     sys.stdout.write(x)
+print("\n")
+
+# split the text
+words = stringinput.split()
+
+    # for each word in the line:
+words.reverse()
+for word in words:
+    sys.stdout.write(word + " ")
